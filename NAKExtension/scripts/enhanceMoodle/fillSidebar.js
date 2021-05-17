3c91ac3cddbe59a26df61cd19b22c03f612ce60a
Yannick Brandt <yannick-brandt@outlook.de>
2021-05-18 00:14:51 +0200

    Refactored fillSideBar

--- NAKExtension/scripts/enhanceMoodle/fillSidebar.js
+++ NAKExtension/scripts/enhanceMoodle/fillSidebar.js
@@ -1,20 +1,19 @@
-function fillSidebar() {
+async function fillSidebar() {
 	const moreButton = document.querySelector(
 		'#nav-drawer > nav > ul > li > a[data-key="courseindexpage"]'
 	)?.parentElement;
 	const nav = moreButton?.parentElement;
 
 	if (nav && moreButton) {
-		start();
-	}
-}
+		const courses = await getCourses();
 
-async function start() {
-	const courses = await getCourses();
-
-	for (let course of courses) {
-		if (!courseAlreadyExists(course.href))
-			addCourseToNav(course.title, course.href);
+		for (let course of courses) {
+			if (!courseAlreadyExists(course.href)) {
+				const courseElm = createCourseElm(course.title, course.href);
+				nav.insertBefore(courseElm, moreButton);
+				console.log("added: " + course.title);
+			}
+		}
 	}
 }
 
@@ -36,19 +35,17 @@ function courseAlreadyExists(link) {
 	);
 }
 
-function addCourseToNav(title, link) {
+function createCourseElm(title, link) {
 	const course = document.createElement("li");
 	course.innerHTML = `
-        <a class="list-group-item list-group-item-action  " href="${link}" data-key="3961" data-isexpandable="1" data-indent="1" data-showdivider="0" data-type="20" data-nodetype="1" data-collapse="0" data-forceopen="0" data-isactive="0" data-hidden="0" data-preceedwithhr="0" data-parent-key="574">
-            <div class="ml-1">
-                <div class="media">
-                    <span class="media-left"><i class="icon fa fa-graduation-cap fa-fw " aria-hidden="true"></i></span>
-                    <span class="media-body ">${title}</span>
-                </div>
-            </div>
-        </a>
-    `;
-
-	nav.insertBefore(course, moreButton);
-	console.log("added: " + title);
+			<a class="list-group-item list-group-item-action  " href="${link}" data-key="3961" data-isexpandable="1" data-indent="1" data-showdivider="0" data-type="20" data-nodetype="1" data-collapse="0" data-forceopen="0" data-isactive="0" data-hidden="0" data-preceedwithhr="0" data-parent-key="574">
+				<div class="ml-1">
+					<div class="media">
+						<span class="media-left"><i class="icon fa fa-graduation-cap fa-fw " aria-hidden="true"></i></span>
+						<span class="media-body ">${title}</span>
+					</div>
+				</div>
+			</a>
+		`;
+	return course;
 }
