de7fe58ac29311f326657d4ad97dfb63234365b0
IceToast <54889359+IceToast@users.noreply.github.com>
2021-05-24 22:51:03 +0200

    Added Notification Permission to extension manifest

--- NAKExtension/background.js
+++ NAKExtension/background.js
@@ -1,7 +1,6 @@
 chrome.runtime.onInstalled.addListener((reason) => {
   // Register an alarm that will wake my background page every hour.
   chrome.alarms.create("nak-exam-notifier", { periodInMinutes: 1 })
-  checkNakExams()
 })
 
 chrome.alarms.onAlarm.addListener((alarm) => {
