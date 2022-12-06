f8670ca207ab1fbe1b6b96ec014012d410aed020
Jan Vollmer <jan@vllmr.dev>
2022-12-06 14:59:06 +0100

    Fix login for CIS
    ID attributes were removed from the login form input fields
    
    fixes #32

--- NAKExtension/scripts/autoLogin/cis.js
+++ NAKExtension/scripts/autoLogin/cis.js
@@ -1,6 +1,6 @@
 function init() {
-	const nameInput = document.getElementById("user");
-	const passInput = document.getElementById("pass");
+	const nameInput = document.querySelector("input[name=user]");
+	const passInput = document.querySelector("input[name=pass]");
 	const loginButton = document.querySelector("input[type=submit]");
 
 	if (nameInput && passInput && loginButton) {
